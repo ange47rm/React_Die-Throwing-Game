106d1f370f21a38845240218829349df3006f31d
ange47rm <ange47rm@hotmail.com>
2022-06-29 15:21:22 +0100

    Basic structure of component created

--- src/components/DieThrowingGameComponent.jsx
+++ src/components/DieThrowingGameComponent.jsx
@@ -1,10 +1,18 @@
 import * as React from 'react';
-
+import Input from '@mui/material/Input';
+import Button from '@mui/material/Button';
 
 export default function DieThrowingGameComponent() {
     return (
-        <>
-        <h1>CIao</h1>
-        </>
+        <div>
+            <form className='centered'>
+                <h3>Number of times to throw the die</h3><br></br>
+                <Input type='number' />
+                <Button variant="contained" size="large" id="submit-btn">Throw Die!</Button>
+            </form>
+
+
+        </div>
+
     );
 }
