f9eea5a966cf81ee6f7e5d8b09ba30ea67345b05
ange47rm <ange47rm@hotmail.com>
2022-06-29 17:25:23 +0100

    Die throw logic implemented and tested

--- src/components/DieThrowingGameComponent.jsx
+++ src/components/DieThrowingGameComponent.jsx
@@ -1,18 +1,44 @@
-import * as React from 'react';
-import Input from '@mui/material/Input';
-import Button from '@mui/material/Button';
+import React, { useState } from 'react';
+import Button from 'react-bootstrap/Button';
+import Form from 'react-bootstrap/Form';
+
 
 export default function DieThrowingGameComponent() {
+
+    const [noOfThrows, setNoOfThrows] = useState();
+    const [winningStreat, setWinningStreat] = useState();
+    
+
+    const handleThrowDie = (event) => {
+        event.preventDefault();
+        throwDie(noOfThrows);
+        event.target.reset();
+    };
+
+    const throwDie = (noOfThrows) => {
+        let results = [];
+        const diePossibleValues = [1, 2, 3, 4, 5, 6];
+
+        for (let i = 0; i < noOfThrows; i++) {
+            const dieResult = diePossibleValues[Math.floor(Math.random() * diePossibleValues.length)];
+            console.log(`Throw No ${i + 1}`);
+        }
+
+        // result = Math.floor(Math.random() * (6 - 1 + 1) ) + 1; // Achieves the same (no need for a list of posible values).
+
+    }
+
     return (
         <div>
-            <form className='centered'>
-                <h3>Number of times to throw the die</h3><br></br>
-                <Input type='number' />
-                <Button variant="contained" size="large" id="submit-btn">Throw Die!</Button>
-            </form>
-
-
+            <Form onSubmit={handleThrowDie}>
+                <Form.Group className="mb-3 centered">
+                    <h2>Number of times to throw the die</h2>
+                    <Form.Control type="number" max="1000000" placeholder="(Example: 24)" onChange={event => setNoOfThrows(event.target.value)} className="input-field m-5"required />
+                </Form.Group>
+                <div className='centered'>
+                    <Button variant="warning" className='button shadow' id='submit-btn' type="submit">Throw Die!<img src="../../dices.png" alt="die-icon" height="80"/></Button>
+                </div>
+            </Form>
         </div>
-
     );
 }
--- src/containers/DieThrowingGameContainer.jsx
+++ src/containers/DieThrowingGameContainer.jsx
@@ -8,7 +8,7 @@ export default function DieThrowingGameContainer() {
   return (
     <>
       <Box sx={{ flexGrow: 1 }}>
-        <AppBar position="static" id="header-bar">
+        <AppBar position="static" id="header-bar" className='mb-5'>
           <Toolbar>
             <img src="https://icons.iconarchive.com/icons/elegantthemes/beautiful-flat-one-color/128/die-icon.png" alt='die_icon'></img>
             <h2>Die Throwing Game</h2>
